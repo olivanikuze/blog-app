b7f25fbac68dfb9843100c0c3877194c0474f81e
olivanikuze <nikuzeol203@gmail.com>
2025-06-20 22:36:41 +0200

    authentication change

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::resource('blogs', PostController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::resource('blogs', PostController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
